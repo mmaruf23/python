48ad92dbcfed8e804aa34a9069c8bfcd1c47f214
muhammadmaruf08 <156484103+muhammadmaruf08@users.noreply.github.com>
2024-03-05 14:52:14 +0700

    update variabel.py

--- variabel.py
+++ variabel.py
@@ -1,7 +1,7 @@
 #variabel itu intinya buat nyimpen data misal
 nama = "Muhammad Maruf"
 print(nama)
-
+print(type(nama))
 #sama aja kaya gini 
 #print("Muhammad Maruf")
 #kalau sama aja terus buat apa dong?
