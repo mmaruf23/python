2b08de31723518a2569abb8f6dd591d238848922
muhammadmaruf08 <156484103+muhammadmaruf08@users.noreply.github.com>
2024-03-05 14:51:08 +0700

    comment

---
+++ variabel.py
@@ -0,0 +1,8 @@
+#variabel itu intinya buat nyimpen data misal
+nama = "Muhammad Maruf"
+print(nama)
+
+#sama aja kaya gini 
+#print("Muhammad Maruf")
+#kalau sama aja terus buat apa dong?
+#jelasin? ga usah lah ya.
