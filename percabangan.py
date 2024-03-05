df73994645e67c64496e9684836d14f80edee815
muhammadmaruf08 <156484103+muhammadmaruf08@users.noreply.github.com>
2024-03-05 15:04:26 +0700

    update percabangan.py

--- percabangan.py
+++ percabangan.py
@@ -1,5 +1,7 @@
 #percabangan itu ya. apa ya .. contohin aja lah ya
 nama = "Maruf"
-usia = 23
-if(usia>20):
- print("Njir si ", nama, "dah tua")
+usia = 18
+if(usia>22):
+  print("Njir si ", nama, "dah tua")
+else:
+  print("Njir Bohong")
